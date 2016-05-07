1750f2377d8b3224de87beaa229cc0330ebd5170
Sergey Glazyrin <sergey.glazyrin.dev@gmail.com>
2016-05-07 11:45:18 +0200

    covered with more tests

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -6,14 +6,18 @@ cur_dir = os.path.dirname(__file__)
 
 
 def test_load_from_directory():
-    with mock.patch('acmdrunner.loader.load_commands_from_directory', autospec=True) as\
-            commands_mock:
+    with mock.patch(
+            'acmdrunner.loader.load_commands_from_directory',
+            autospec=True
+    ) as commands_mock:
         Loader.load_from_directory(cur_dir)
         commands_mock.assert_called_once_with(cur_dir)
 
 
 def test_load_from_package():
-    with mock.patch('acmdrunner.loader.load_commands_from_directory', autospec=True) as\
-            commands_mock:
+    with mock.patch(
+            'acmdrunner.loader.load_commands_from_directory',
+            autospec=True
+    ) as commands_mock:
         Loader.load_from_package('tests')
         commands_mock.assert_called_once_with(cur_dir, package='tests')
